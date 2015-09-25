f946063c3fd57ef27cb84befecf3ae22f6bf73c7
Kin3Xc <kin3xc@hotmail.com>
2015-09-25 13:17:29 -0500

    Portafolio de oglit

---
+++ php/contact-form/php/contact.php
@@ -0,0 +1,45 @@
+<?php
+	//Script PHP que se ejecuta en el server y envia el Mail
+	// $frm = $_POST['frm'];
+
+	// if (isset($frm) && $frm !== '') {
+		// if ($frm == 'contact') {
+			$Nombre = $_POST['username'];
+			$Email = $_POST['useremail'];
+			$Mensaje = $_POST['usermessage'];
+
+			$Destino = 'kin3xc@hotmail.com';
+
+			$header1 = "From: $Email".PHP_EOL;
+			$header1 .= 'Content-type: text; charset=UTF-8' . "\r\n";
+
+			// $mensaje = $Mensaje;
+			$Mensaje.= "\n\n------------------------";
+			$Mensaje.= "\nEnviado por: ". $Nombre;
+			$Mensaje.= "\nCorreo: " . $Email;
+
+			//Ingresamos nuestra casilla de correo
+			$Asunto = $_POST['usersubject'];
+
+			if(mail($Destino, $Asunto, $Mensaje, $header1)){
+				$respuesta = 'success';
+				
+				//enviar confirmación al usuario
+				$header = "From: kin3xc@hotmail.com".PHP_EOL;
+				$header .= 'Content-type: text; charset=UTF-8' . "\r\n";
+
+				$Asunto1 = "Oglit.com - Mensaje enviado";
+				$mensajeUser = "Su mensaje fue enviado con éxito.\nPronto nos pondremos en contacto con usted\n\n\nCordialmente,\nhttp://oglit.com";
+				mail($Email, $Asunto1, $mensajeUser, $header);
+			}else{
+				$respuesta = false;
+			}
+
+		// }else{
+		// 	$respuesta = false;
+		// }
+		// header('Content-type: application/json; charset=utf-8');
+		// echo json_encode($respuesta, JSON_FORCE_OBJECT);
+		echo $respuesta;
+	// }
+?>
